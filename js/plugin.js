113a1615430859fe3f7db9499f4efb6ac931d167
Gabriel Trisca <tog000@gmail.com>
2014-05-12 16:41:28 -0600

    cleanup

--- js/plugin.js
+++ js/plugin.js
@@ -1,30 +1,10 @@
-tinymce.PluginManager.add('example', function(editor, url) {
+tinymce.PluginManager.add('question', function(editor, url) {
 	// Add a button that opens a window
-	editor.addButton('example', {
+	editor.addButton('question', {
 		text: 'Question',
 		icon: 'bullist',
 		onclick: function() {
-			// Open window
 			openWindow(editor);
-		}
-	});
-
-	// Adds a menu item to the tools menu
-	editor.addMenuItem('example', {
-		text: 'Example plugin',
-		context: 'tools',
-		onclick: function() {
-			// Open window with a specific url
-			editor.windowManager.open({
-				title: 'TinyMCE site',
-				url: 'http://www.tinymce.com',
-				width: 800,
-				height: 600,
-				buttons: [{
-					text: 'Close',
-					onclick: 'close'
-				}]
-			});
 		}
 	});
 });
@@ -32,21 +12,27 @@ tinymce.PluginManager.add('example', function(editor, url) {
 function openWindow(editor){
 
 	editor.windowManager.open({
-		title: 'Example plugin',
+		title: 'question plugin',
 		body: [
-		{type: 'textbox', name: 'title', label: 'Title', value:'Test'}
+			{type: 'textbox', name: 'title1', label: 'Title', value:'Test'},
+			{type: 'radio', name: 'title', label: 'Title', value:'Test'},
+			{type: 'textbox', name: 'title2', label: 'Title', value:'Test'},
+			{type: 'checkbox', name: 'c', label: 'Title', value:'Test'},
+			{type: 'textbox', name: 'title3', label: 'Title', value:'Test'},
+			{type: 'checkbox', name: 'c', label: 'Title', value:'Test'},
 		],
 		onsubmit: function(e) {
 				var target = editor.dom.select("#q1");
 				console.log("Target:"+target)
 				if(target.length==0){
 					
-					editor.dom.add(tinyMCE.activeEditor.getBody(), 'div', {contentEditable:'false', id : 'q1', 'class':"question mceNonEditable"}, e.data.title);
+					editor.dom.add(tinyMCE.activeEditor.getBody(), 'question', {contentEditable:'false', id : 'q1', 'class':"question mceNonEditable"}, e.data.title);
 					editor.dom.add(tinyMCE.activeEditor.getBody(), 'br', {}, "");
 					
 					editor.dom.bind(tinymce.activeEditor.dom.select("#q1"), "click", function(){
 						openWindow(editor)
 					}, this);
+
 				}else{
 					console.log(target[0].innerHTML)
 					target[0].innerHTML = e.data.title;
